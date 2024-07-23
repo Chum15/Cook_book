dfb197b025f6638579257a27174c019e60b8e374
Igor Balabanov <chum6muhc@gmail.com>
2024-07-23 15:17:51 +0700

    removed the global context

--- cook_book.py
+++ cook_book.py
@@ -1,37 +1,38 @@
 from pprint import pprint
 from collections import Counter
 
-with open("recipes.txt", encoding='utf-8') as f:
-    ST_TITLE = 1
-    ST_COUNT = 2
-    ST_INGREDIENTS = 3
-
-    cook_book = {}
-    state = ST_TITLE
-
-    for line in f:
-        line = line.strip()
-        if not line: continue
-        if state == ST_TITLE:
-            title = line
-            cook_book[title] = []
-            state = ST_COUNT
-        elif state == ST_COUNT:
-            count = int(line)
-            state = ST_INGREDIENTS
-        elif state == ST_INGREDIENTS:
-            data = [x.strip() for x in line.split('|')]
-            data[1] = int(data[1])
-            cook_book[title].append(dict(zip(('ingredient_name', 'quantity', 'measure'), data)))
-            count -= 1
-            if count == 0:
-                state = ST_TITLE
-    
-pprint(cook_book)
 def get_shop_list_by_dishes(dishes, person_count):
+    with open("recipes.txt", encoding='utf-8') as f:
+        ST_TITLE = 1
+        ST_COUNT = 2
+        ST_INGREDIENTS = 3
+
+        cook_book = {}
+        state = ST_TITLE
+
+        for line in f:
+            line = line.strip()
+            if not line: continue
+            if state == ST_TITLE:
+                title = line
+                cook_book[title] = []
+                state = ST_COUNT
+            elif state == ST_COUNT:
+                count = int(line)
+                state = ST_INGREDIENTS
+            elif state == ST_INGREDIENTS:
+                data = [x.strip() for x in line.split('|')]
+                data[1] = int(data[1])
+                cook_book[title].append(dict(zip(('ingredient_name', 'quantity', 'measure'), data)))
+                count -= 1
+                if count == 0:
+                    state = ST_TITLE
+        
+    pprint(cook_book)
+
     counter = Counter(dishes)
     counter = dict(counter)
-  
+    
     dish = {}
     for d in dishes:    
         dishes = cook_book[d]
@@ -45,5 +46,5 @@ def get_shop_list_by_dishes(dishes, person_count):
                 dish[key]['quantity'] = i
 
     return dish 
-       
+    
 pprint(get_shop_list_by_dishes(['Омлет', 'Омлет','Утка по-пекински','Утка по-пекински','Омлет'], 2))
